414119f2cf0defb45563ebd834ac5f52a97a0113
Chase Kapler <chasekapler@Chases-MBP.attlocal.net>
2022-02-24 16:53:18 -0800

    fixed suns

--- Suns.js
+++ Suns.js
@@ -6,15 +6,20 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
 
-const {Cube, Axis_Arrows, Textured_Phong,Subdivision_Sphere} = defs
+const {Cube, Phong_Shader,Subdivision_Sphere} = defs
 
 export class Suns {
     constructor() {
 
     this.DIST_FROM_CENTER = 10
-    this.DIST_FROM_PLANET = 20
+    this.DIST_FROM_PLANET = 25 
     this.SUN_VELO = 0.002
     this.CENTER_VELO = 0.0005
+    this.material = new Material(new Phong_Shader, {
+        ambient: 0.8,
+        diffusivity: 0, 
+        specularity: 0
+    })
 
     this.orbitCenter = {
         dist_from_planet: this.DIST_FROM_PLANET, 
@@ -28,7 +33,7 @@ export class Suns {
     }
 
     this.sun1 = {
-        size: 6, 
+        size: 8, 
         color: "#FFFFFF", 
         light: {brightness: 1000}, 
         dist_from_center: this.DIST_FROM_CENTER, 
@@ -70,7 +75,8 @@ export class Suns {
             color: sun.color, 
             size: sun.size, 
             light: sun.light, 
-            shape: sun.shape
+            shape: sun.shape, 
+            material: this.material
         }
     }
 
