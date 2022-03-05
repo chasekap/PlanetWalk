de3254fffb382a8196e4957e1e2ed8a94d4033f6
Chase Kapler <chasekapler@Chases-MBP.attlocal.net>
2022-03-05 12:03:23 -0800

    added variable particle effect

--- PlanetWalk.js
+++ PlanetWalk.js
@@ -46,10 +46,15 @@ export class PlanetWalk extends Scene {
 
   
     drawShapes(listOfShapes, context, program_state, model_transform){
+       
         listOfShapes.forEach((shape) => {
 
             if (shape.light){
                 program_state.lights.push(new Light(vec4(shape.x,shape.y,shape.z, 1), color(1, 1, 1, 1), 1000 ));
+            }
+            if (shape.trail){
+                console.log(shape.trail)
+                this.drawShapes(shape.trail, context,program_state,model_transform)
             }
 
             model_transform = model_transform.times(Mat4.translation(shape.x,shape.y,shape.z));
--- ShootingStars.js
+++ ShootingStars.js
@@ -11,6 +11,9 @@ const {Cube, Phong_Shader, Subdivision_Sphere} = defs
 export class ShootingStars{
     constructor() {
     this.SPAWN_RATE = 0.5 / 30
+    this.PHI_VARIATION = 0
+    this.THETA_VARIATION = 0.01
+    this.TRAIL_RATE = 0.9
     this.PLANET_RADIUS = 1 
     this.Velocity = 0.1
     this.atmosphere_radius = 5
@@ -33,8 +36,10 @@ export class ShootingStars{
                r: this.atmosphere_radius, 
                theta: Math.random() * Math.PI * 2 , 
                phi: Math.random() * Math.PI, 
-               color: Color.of(Math.max(Math.random(), 0.9), Math.max(Math.random(), 0.9),Math.max(Math.random(), 0.9),1)
+               color: Color.of(Math.max(Math.random(), 0.9), Math.max(Math.random(), 0.9),Math.max(Math.random(), 0.9),1), 
+               
            }
+           star.trail =  this.generateTrail(star.r,star.theta,star.phi)
            this.ShootingStars.push(star)
        }
     }
@@ -42,8 +47,43 @@ export class ShootingStars{
         
         this.ShootingStars = this.ShootingStars.filter(star => {return this.PLANET_RADIUS < star.r})
         .map(star => {
-            return {...star, r: star.r - this.Velocity}
+            return {...star, r: star.r - this.Velocity, trail: this.moveTrail(star)}
         })
+    }
+    moveTrail(star){
+        return star.trail.map(particle => {
+            const r = particle.r - this.Velocity
+            const phi =  particle.phi + this.PHI_VARIATION * Math.random() - this.PHI_VARIATION * Math.random();
+            const theta =  particle.theta + this.THETA_VARIATION * Math.random() - this.THETA_VARIATION * Math.random();
+            return {...particle, r: r, phi: phi, theta: theta,
+                x: r * Math.cos(phi) * Math.sin(theta), 
+                y: r * Math.sin(phi) * Math.sin(theta), 
+                z: r * Math.cos(theta) }
+        })
+    }
+    generateTrail(r,theta,phi){
+        let trail = []
+        for (let i = 0; i < 100; i++){
+            if (Math.random() < this.TRAIL_RATE){
+                r += 0.02 * Math.random(); 
+                theta += this.THETA_VARIATION * Math.random() - this.THETA_VARIATION * Math.random();
+                phi += this.PHI_VARIATION * Math.random() - this.PHI_VARIATION * Math.random();
+                const trail_particle = {
+                r: r,
+                phi: phi, 
+                theta: theta, 
+                x: r * Math.cos(phi) * Math.sin(theta), 
+                y: r * Math.sin(phi) * Math.sin(theta), 
+                z: r * Math.cos(theta), 
+                color: Color.of(Math.max(Math.random(), 0.7), Math.max(Math.random(), 0.7),Math.max(Math.random(), 0.7),1),
+                shape: this.shapes.star, 
+                material: this.material, 
+                size: 0.008, }
+                trail.push(trail_particle);
+            }
+        }
+        return trail
+
     }
     convertStarsToXYZShape(){
         return this.ShootingStars.map(star => {
@@ -55,7 +95,9 @@ export class ShootingStars{
                 shape: this.shapes.star, 
                 material: this.material, 
                 light: true,
-                size: 0.1
+                size: 0.1, 
+                trail: star.trail
+                
             }
         })
     }
